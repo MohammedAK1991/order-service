96a36b09eb258c5384510ccad1c2fbf5885f2599
mohammedAK1991 <mohmed.ak.17@gmail.com>
2024-09-27 18:14:58 +0200

    feat(DB): providers and modules for mongoose database operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [OrderModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, OrderModule],
 })
 export class AppModule {}
