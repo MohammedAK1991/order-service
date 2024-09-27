b458e2c173d1b75738b5dac1d5a36de275fbfba7
mohammedAK1991 <mohmed.ak.17@gmail.com>
2024-09-27 23:48:25 +0200

    refactor(order): Update CreateOrderDto validation decorators

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,8 +1,29 @@
+import { IsNotEmpty, IsString, IsNumber, IsPositive } from 'class-validator';
+
 export class CreateOrderDto {
+  @IsNotEmpty()
+  @IsString()
   orderId: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   quantity: number;
+
+  @IsNotEmpty()
+  @IsString()
   productId: string;
+
+  @IsNotEmpty()
+  @IsString()
   customerId: string;
+
+  @IsNotEmpty()
+  @IsString()
   sellerId: string;
 }
