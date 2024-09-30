c161fa5a0700e6a1b7ddc78cea5509ac6aff47eb
mohammedAK1991 <mohmed.ak.17@gmail.com>
2024-09-30 13:31:29 +0200

    refactor: Update main.ts to load environment variables using dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe());
